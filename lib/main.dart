61b2f6411f33e1a50484a44e1c09a432d549a65c
Lyudmila Kovtun <laudylim@yandex.ru>
2021-10-12 00:20:34 +0300

    T - hotreload added

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter tutorial',
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello world'),
-        backgroundColor: Colors.black87,
-      ),
-      body: const Text(
-        'Hello world',
-        style: TextStyle(
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.black87,
-          fontFamily: 'JetBrainsMono',
-          ),
-        )
-      // floatingActionButton: FloatingActionButton(
-      //   child: Text('Add'),
-      //   onPressed: ,
-      // ),
-      // bottomNavigationBar: BottomNavigationBar(
+  runApp(const FlutterTutApp());
+}
 
-      // ),
-    ),
-  ));
+class FlutterTutApp extends StatelessWidget {
+  const FlutterTutApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter tutorial',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello world'),
+          backgroundColor: Colors.black87,
+        ),
+        body: const Text(
+          'Hello world',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.red,
+            fontFamily: 'JetBrainsMono',
+            ),
+          )
+        // floatingActionButton: FloatingActionButton(
+        //   child: Text('Add'),
+        //   onPressed: ,
+        // ),
+        // bottomNavigationBar: BottomNavigationBar(
+
+        // ),
+      ),
+    );
+  }
 }
