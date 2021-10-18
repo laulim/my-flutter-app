468ad79e023172eafc64114c365fe9fa6bd2feb6
Lyudmila Kovtun <laudylim@yandex.ru>
2021-10-18 18:16:10 +0300

    T - icons and paddings

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 
 import 'package:flutter/material.dart';
+import 'package:my_app/custom_icons.dart';
 
 void main() {
   runApp(const FlutterTutApp());
 }
+
+var icon = IconButton(
+            icon: const Icon(
+              // Icons.search,
+              MyFlutterApp.clover,
+              // size: 100.0,
+            ),
+            iconSize: 50.0,
+            color: Colors.green,
+            onPressed: () {
+              print('wow');
+            },
+          );
 
 class FlutterTutApp extends StatelessWidget {
   const FlutterTutApp({Key? key}) : super(key: key);
@@ -17,21 +31,17 @@ class FlutterTutApp extends StatelessWidget {
           title: const Text('Hello world'),
           backgroundColor: Colors.black87,
         ),
-        body: IconButton(
-            icon: const Icon(
-            Icons.search,
-            // size: 100.0,
-          ),
-          iconSize: 50.0,
-          color: Colors.red,
-          onPressed: () {
-            print('wow');
-          },
-        )
-        // floatingActionButton: FloatingActionButton(
-        //   child: Text('Add'),
-        //   onPressed: ,
-        // ),
+        body: Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: icon,
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Add'),
+          onPressed: () => {print('add')},
+        ),
         // bottomNavigationBar: BottomNavigationBar(
 
         // ),
